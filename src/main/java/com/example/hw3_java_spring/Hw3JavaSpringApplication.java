47665c2b46b0f6f3e3520d495b95ce99ba1cc296
John <121889006+jgw0915@users.noreply.github.com>
2023-11-23 16:10:45 +0800

    Update Hw3JavaSpringApplication.java

--- src/main/java/com/example/hw3_java_spring/Hw3JavaSpringApplication.java
+++ src/main/java/com/example/hw3_java_spring/Hw3JavaSpringApplication.java
@@ -1,16 +1,16 @@
-package com.example.demo;
+package com.example.hw3_java_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class Hw3JavaSpringApplication {
 	@GetMapping("/")
 	public String hello() {
 		return "<html><body><h1>Hello DevSecOps</body></h1></html>";
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Hw3JavaSpringApplication.class, args);
 	}
 }
