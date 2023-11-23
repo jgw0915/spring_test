5b226b70df9d3f1ebc53aa0d450b0d61f52ecf91
jgw0915 <121889006+jgw0915@users.noreply.github.com>
2023-11-23 15:20:49 +0800

    first commit

---
+++ src/test/java/com/example/hw3_java_spring/Hw3JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw3_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
